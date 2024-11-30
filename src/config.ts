4adece3d4504f1fc596ecdad63ae18e4f29fb356
Matej Gerek <matej.gerek@student.tuke.sk>
2024-11-30 19:42:54 +0100

    🔧 Update LIBRE_LINK_API_URL to point to the EU API endpoint and format config object for better readability

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-
-const LIBRE_LINK_API_URL = process?.env?.LIBRE_LINK_API_URL ?? "https://api-us.libreview.io";
+const LIBRE_LINK_API_URL =
+  process?.env?.LIBRE_LINK_API_URL ?? "https://api-eu.libreview.io";
 const LIBRE_LINK_EMAIL = process?.env?.LIBRE_LINK_EMAIL;
 const LIBRE_LINK_PASSWORD = process?.env?.LIBRE_LINK_PASSWORD;
 const LIBRE_LINK_UP_VERSION = process?.env?.LIBRE_LINK_UP_VERSION ?? "4.7.0";
@@ -10,12 +10,12 @@ const VERBOSE = process?.env?.VERBOSE === "true";
  * @description Configuration for the Libre Link Up API client.
  */
 export const config = Object.freeze({
-    apiUrl: LIBRE_LINK_API_URL,
-    patientId: LIBRE_LINK_PATIENT_ID,
-    credentials: {
-        email: LIBRE_LINK_EMAIL,
-        password: LIBRE_LINK_PASSWORD
-    },
-    lluVersion: LIBRE_LINK_UP_VERSION,
-    verbose: VERBOSE
+  apiUrl: LIBRE_LINK_API_URL,
+  patientId: LIBRE_LINK_PATIENT_ID,
+  credentials: {
+    email: LIBRE_LINK_EMAIL,
+    password: LIBRE_LINK_PASSWORD,
+  },
+  lluVersion: LIBRE_LINK_UP_VERSION,
+  verbose: VERBOSE,
 });
